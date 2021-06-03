9bf710ca750bdabe95fd34710b3299e3e1e91311
javiers1999 <56318901+javiers1999@users.noreply.github.com>
2021-06-03 19:45:39 -0400

    remove from branch gh-pages

--- src/app/back-office/back-office.component.spec.ts
+++ src/app/back-office/back-office.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { InicioComponent } from './inicio.component';
+import { BackOfficeComponent } from './back-office.component';
 
-describe('InicioComponent', () => {
-  let component: InicioComponent;
-  let fixture: ComponentFixture<InicioComponent>;
+describe('BackOfficeComponent', () => {
+  let component: BackOfficeComponent;
+  let fixture: ComponentFixture<BackOfficeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ InicioComponent ]
+      declarations: [ BackOfficeComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(InicioComponent);
+    fixture = TestBed.createComponent(BackOfficeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
