371889b33bcc00ac6a18643a9c1041081f9ff558
javiers1999 <56318901+javiers1999@users.noreply.github.com>
2021-06-04 17:58:13 -0400

    routers basicos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { InicioComponent } from '../app/inicio/inicio.component';
+import { RegistroComponent } from '../app/registro/registro.component';
+
+const routes: Routes = [
+  {path:'',component:InicioComponent},
+  {path:'registro',component:RegistroComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup , FormBuilder , Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  formularioRegistro:FormGroup;
+  nombres:any;
+  apellidos:any;
+  rut:any;
+  pass:any;
+  correo:any;
+  direccion:any;
+  region:any;
+  comuna:any;
+
+  constructor(public frs:FormBuilder) {
+    this.formularioRegistro = this.frs.group({
+      nombre:['',Validators.required],
+      apellidos:['',Validators.required],
+      rut:['',Validators.required],
+      pass:['',Validators.required],
+      correo:['',Validators.required],
+      direccion:['',Validators.required],
+      region:['',Validators.required],
+      comuna:['',Validators.required]
+    });
+  }
 
   ngOnInit(): void {
+    this.nombres = this.formularioRegistro.get("nombres") as FormGroup;
+    this.apellidos = this.formularioRegistro.get("apellidos") as FormGroup;
+    this.rut = this.formularioRegistro.get("rut") as FormGroup;
+    this.pass = this.formularioRegistro.get("pass") as FormGroup;
+    this.correo = this.formularioRegistro.get("correo") as FormGroup;
+    this.direccion = this.formularioRegistro.get("direccion") as FormGroup;
+    this.region = this.formularioRegistro.get("region") as FormGroup;
+    this.comuna = this.formularioRegistro.get("comuna") as FormGroup;
   }
 
 }
